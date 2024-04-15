850971f64335efae9d200c2fdd269277cc04c2dd
Afrazsheikh <sheikhafraz04@gmail.com>
2024-04-15 22:51:23 +0530

    css

--- src/app/components/home-page/about-us/about-us.component.ts
+++ src/app/components/home-page/about-us/about-us.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
+  type: any;
 
+  ngOnInit() {
+    if (typeof localStorage !== 'undefined') {
+      this.type = localStorage.getItem('language');
+      console.log(this.type);
+    }
+  }
+
+  isDarkModeInLocalStorage(): boolean {
+    if (typeof localStorage !== 'undefined') {
+      const isDark = localStorage.getItem('darkMode');
+      return isDark === 'true';
+    } else {
+      return false;
+    }
+  }
 }
