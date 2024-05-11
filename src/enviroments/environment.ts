038bf342de936e2d660917be316d0714fe661dda
Aniket Verma <62088467+aniketverma11@users.noreply.github.com>
2024-05-11 16:25:02 +0530

    Update environment.ts

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://3.27.228.84:8000/api/v1/',
-  imagesBaseURL: 'http://3.27.228.84:8000/',
+  apiBaseUrl: 'http://54.253.115.2:8000/api/v1/',
+  imagesBaseURL: 'http://54.253.115.2:8000/',
   google_client_id:
     '285264854894-kg4d4ac57oeh6pv78rej7p8amh8tsjal.apps.googleusercontent.com',
   webiste_url: 'http://localhost:4200/',
