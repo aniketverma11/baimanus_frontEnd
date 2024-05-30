9e451ed0bd23302a17674715cd5e35eed86a2cb0
Aniket Verma <62088467+aniketverma11@users.noreply.github.com>
2024-05-31 00:24:02 +0530

    Update environment.ts

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://54.253.115.2:8000/api/v1/',
-  imagesBaseURL: 'http://54.253.115.2:8000/',
+  apiBaseUrl: 'https://api.baimanus.in/api/v1/',
+  imagesBaseURL: '',
   google_client_id:
     '285264854894-kg4d4ac57oeh6pv78rej7p8amh8tsjal.apps.googleusercontent.com',
   webiste_url: 'http://localhost:4200/',
